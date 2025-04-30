714586cb4e749e06690b7f893de17007fdade418
felipewnoob <massinifelipe04@gmail.com>
2025-04-29 21:02:13 -0300

    atualizacao2.0

---
+++ src/components/Corpo.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Corpo.css'
+
+function Corpo() {
+  
+function falaMarcia(){
+
+}
+  
+
+  
+    return (
+    <div>
+      
+    <div className="container-corpo">
+        <div>
+            <button onClick={falaMarcia}>HITORIA DA FAR-MACIA</button>
+        </div>
+
+
+
+
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Corpo
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div>
+      
+      
+      <div className="container-navbar">
+        <h1>
+            Farmacia da Márcia 
+        </h1>
+
+
+      </div>
+    
+    
+    
+    </div>
+  )
+}
+
+export default Navbar
